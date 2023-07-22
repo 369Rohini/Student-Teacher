b6c7966e5ff4df3f6c9923c29ee7508801cf7646
369Rohini <111850589+369Rohini@users.noreply.github.com>
2023-07-22 13:19:53 +0530

    Update forms.py

--- project/app1/forms.py
+++ project/app1/forms.py
@@ -8,9 +8,20 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
 
-class SignupForm(forms.ModelForm):
+# class SignupForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+
+from django.contrib.auth.models import User
+
+class SignUpForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
 
     class Meta:
         model = CustomUser
