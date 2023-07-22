bbc3c111ec4f6a57ff40e126bc044b544592580b
Rohini <rohinirana369@gmail.com>
2023-07-22 11:47:35 +0530

    Your commit message here

---
+++ project/app1/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+#CustomUser represents both teacher as well as student
+class CustomUser(AbstractUser):
+    pass
+
+class Announcement(models.Model):
+    teacher_name = models.CharField(max_length=100)
+    message = models.TextField()
+
+def __str__(self):
+    return self.teacher_name
+
+class Notification(models.Model):
+    teacher_name = models.CharField(max_length=100)
+    message = models.TextField()
+    student_username = models.CharField(max_length=100)
---
+++ project/project/urls.py
@@ -0,0 +1,35 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from app1 import views
+from django.contrib import admin
+
+urlpatterns = [
+    # ...
+    path('admin/',admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('teacher/', views.teacher_page, name='teacher_page'),
+    path('student/', views.student_page, name='student_page'),
+    path('',views.homepage,name='homepage'),
+    path('record_action/', views.record_action, name='record_action'),
+    path('fetch_notifications/', views.fetch_notifications, name='fetch_notifications'),
+    path('fetch_viewed_messages/', views.fetch_viewed_messages, name='fetch_viewed_messages'),
+    
+
+    # ...
+]
+
