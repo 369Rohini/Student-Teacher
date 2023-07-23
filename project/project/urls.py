a83e23e09bd9cf63d9381b3df8bf0cf9a46a0671
369Rohini <111850589+369Rohini@users.noreply.github.com>
2023-07-24 01:06:12 +0530

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,11 +20,11 @@ from django.contrib import admin
 urlpatterns = [
     # ...
     path('admin/',admin.site.urls),
+    path('',views.homepage,name='homepage'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    path('teacher/', views.teacher_page, name='teacher_page'),
-    path('student/', views.student_page, name='student_page'),
-    path('',views.homepage,name='homepage'),
+    path('teacher/', views.teacher_page, name='teacher'),
+    path('student/', views.student_page, name='student'),
     path('record_action/', views.record_action, name='record_action'),
     # path('fetch_notifications/', views.fetch_notifications, name='fetch_notifications'),
     # path('fetch_viewed_messages/', views.fetch_viewed_messages, name='fetch_viewed_messages'),
