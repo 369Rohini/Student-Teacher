2610be256a644d5513b3c832f2dfcac09ac7f61b
Sunil Shetye <sunilshetye@rocketmail.com>
2023-07-25 10:38:54 +0530

    make notification working

--- project/app1/models.py
+++ project/app1/models.py
@@ -1,60 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 
-#CustomUser represents both teacher as well as student
-# class CustomUser(AbstractUser):
-#     pass
-
-class User(AbstractUser): 
-    username =models.CharField(max_length=100,unique= True, null=False, blank=False)
-    name = models.CharField(max_length=100,default='Default Name')
-    password = models.CharField(max_length=100)
-    # REQUIRED_FIELDS=(user_name)
+class User(AbstractUser):
     ROLE_CHOICES = (
         ('student', 'Student'),
         ('teacher', 'Teacher'),
     )
-    
+
+    username = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=100, default='Default Name')
+    password = models.CharField(max_length=100)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
     def __str__(self):
         return self.name
-    
-
-
-
-class Teacher(User):
-    pass
-
-
-
-class Student(User):
-    pass
-
-class Admin(User):
-     pass
-
-# class Announcement(models.Model):
-#     teacher_name = models.CharField(max_length=100)
-#     timestamp= models.DateTimeField(default= datetime.now)
-#     message = models.TextField()
-
-
-
-# class Notification(models.Model):
-#     teacher_name = models.CharField(max_length=100)
-#     message = models.TextField()
-#     student_username = models.CharField(max_length=100)
 
 
 class Announcement(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT, default=None)
-    student = models.ForeignKey(Student, on_delete=models.PROTECT, default=None)
-
-
-    acknowledgement= models.BooleanField(default=False)
-    timestamp= models.DateTimeField(default= datetime.now)
-    # teacher_name= models.CharField(max_length=100,default='', null=True, blank=True)
-
+    teacher = models.ForeignKey(User, on_delete=models.PROTECT, default=None)
     message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.teacher}: {self.message}"
+
+
+class Notification(models.Model):
+    student = models.ForeignKey(User, on_delete=models.PROTECT, default=None)
+    announcement = models.ForeignKey(Announcement, on_delete=models.PROTECT, default=None)
+    acknowledgement = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(default=None, null=True)
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,18 +18,13 @@ from app1 import views
 from django.contrib import admin
 
 urlpatterns = [
-    # ...
-    path('admin/',admin.site.urls),
-    path('',views.homepage,name='homepage'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('teacher/', views.teacher, name='teacher'),
-    path('student/', views.student, name='student'),
+    path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
+    path('signup/', views.signup_page, name='signup'),
+    path('login/', views.login_page, name='login'),
+    path('teacher/', views.teacher_page, name='teacher'),
+    path('student/', views.student_page, name='student'),
     path('record_action/', views.record_action, name='record_action'),
     # path('fetch_notifications/', views.fetch_notifications, name='fetch_notifications'),
     # path('fetch_viewed_messages/', views.fetch_viewed_messages, name='fetch_viewed_messages'),
-    
-
-    # ...
 ]
-
